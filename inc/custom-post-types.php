fb16ce23e043cd3df238f79c6469008d941ba80e
matheusgimenez <contato@matheusgimenez.com.br>
2018-05-29 23:15:16 -0300

    issue #53

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -4,7 +4,7 @@
  */
 require_once get_template_directory() . '/core/classes/class-post-type.php';
 
-// CPTs Colunistas
+// CPT Colunistas
 $colunistas = new Odin_Post_Type(
     'Colunista', // Nome (Singular) do Post Type.
     'colunistas' // Slug do Post Type.
@@ -13,5 +13,24 @@ $colunistas->set_arguments(
     array(
         'supports' => array( 'title', 'editor', 'thumbnail' ),
         'menu_icon' => 'dashicons-groups'
+    )
+);
+
+// CPT Especiais
+$especiais = new Odin_Post_Type(
+    'Especiais', // Nome (Singular) do Post Type.
+    'especiais' // Slug do Post Type.
+);
+$especiais->set_arguments(
+    array(
+        'supports' => array( 'title', 'editor', 'thumbnail' ),
+        'menu_icon' => 'dashicons-star-filled'
+    )
+);
+$especiais->set_labels(
+    array(
+        'menu_name' => 'Especiais',
+        'name'		=> 'Especiais',
+        'all_items'	=> 'Ver todos especiais'
     )
 );
