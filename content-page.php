fe8befe630ad065f33c1727d9027837a0b98f187
willowmagrini <brmagrini@gmail.com>
2018-06-01 14:31:16 -0300

    acerta algum css

--- content-page.php
+++ content-page.php
@@ -6,7 +6,7 @@
  * @since 2.2.0
  */
 ?>
-
+<?php the_post_thumbnail( $size = 'full'); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' ); ?>
 
