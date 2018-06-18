efdb3efcee04d35dffcf120b724b69046624eb5f
willowmagrini <brmagrini@gmail.com>
2018-06-18 11:15:41 -0300

    modifica single especial para adicionar a widget área

--- single-especiais.php
+++ single-especiais.php
@@ -5,29 +5,15 @@
  * @package Odin
  * @since 2.2.0
  */
-
-get_header('large'); ?>
-
-	<div id="primary" class="<?php echo odin_classes_page_sidebar(); ?>">
-		<main id="main-content" class="site-main" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( '/content/content', 'especiais' );
-
-					// Social area (share button, comments, etc )
-					get_template_part( '/parts/social-area' );
-				endwhile;
-			?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+ get_header( 'large' ); ?>
+ 	<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
+ 		<div class="col-md-12 no-padding">
+ 			<?php
+			while ( have_posts() ) : the_post();
+				dynamic_sidebar( eol_get_widget_object_id() );
+			endwhile;
+		?>
+ 		</div><!-- .col-md-12 -->
+ 	</main><!-- #main -->
+ <?php
+ get_footer();
