d0e0c8983b39fad4b005dc1f6e169960a0e11a13
bruno magrini <brmagrini@gmail.com>
2018-06-28 11:26:22 -0300

    adiciona widget de tags

--- inc/widgets/class-widget-tags.php
+++ inc/widgets/class-widget-tags.php
@@ -33,36 +33,27 @@ class EOL_Tags_Widget extends WP_Widget {
 		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
 
 		// pega o termo da tag selecionada;
-		$tags_selecionadas = esc_attr( $instance[ 'tags_selecionadas' ] );
+		if (isset($instance[ 'tags_selecionadas' ])) {
+			$tags_selecionadas = esc_attr( $instance[ 'tags_selecionadas' ] );
+			$array_tags = explode(",", $tags_selecionadas);
+			?>
+			<span class="tag-links">
+			<?php
+			foreach ($array_tags as $tag) {
+				$tag_obj = get_term_by( 'name', $tag, 'post_tag', $output = OBJECT, $filter = 'raw' );
+				$tag_link = get_term_link( $tag_obj );
+		    // If there was an error, continue to the next term.
+		    if ( is_wp_error( $tag_link ) ) {
+		        continue;
+		    }
 
-		$query = new WP_Query(
-			array(
-				'posts_per_page' => $number,
-				'post_type' => array('post', 'especiais'),
-				'tax_query' => array(
-					array(
-						'taxonomy' => '_featured_eo',
-						'terms'    => $posicao,
-					),
-				),
-			)
-		);
-		if ( $query->have_posts() ) {
-			printf( '<div class="widget-eol-posts widget-container %s">', esc_attr( $instance[ 'classes_widget' ] ) );
-			if ( ! empty( $title ) ) {
-				echo $args['before_title'] . $title . $args['after_title'];
+		    // We successfully got a link. Print it out.
+		    echo '<a href="' . esc_url( $tag_link ) . '">' . $tag_obj->name . '</a>';
 			}
-			// coloca o widget atual numa variavel global para
-			$GLOBALS[ 'current_widget' ] = $instance;
-			while( $query->have_posts() ) {
-				$query->the_post();
-				get_template_part( 'content/post' );
-			}
-			echo '</div>';
-			wp_reset_postdata();
+			?>
+			</span>
+			<?php
 		}
-		?>
-		<?php
 	}
 
 	/**
@@ -78,13 +69,7 @@ class EOL_Tags_Widget extends WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['title'] = sanitize_text_field( $new_instance['title'] );
-		$instance['classes_widget'] = esc_attr( sanitize_text_field( $new_instance['classes_widget'] ) );
-		$instance['classes_posts'] = esc_attr( sanitize_text_field( $new_instance['classes_posts'] ) );
-		$instance[ 'posicao' ] = absint( $new_instance[ 'posicao'] );
-		$instance['readmore'] = sanitize_text_field( $new_instance['readmore'] );
-		$instance[ 'number' ] = absint( $new_instance[ 'number'] );
-
+		$instance['tags_selecionadas'] = sanitize_text_field( $new_instance['tags_selecionadas'] );
 		return $instance;
 	}
 
@@ -99,97 +84,20 @@ class EOL_Tags_Widget extends WP_Widget {
 	public function form( $instance ) {
 		$instance = wp_parse_args( (array) $instance,
 			array(
-				'title' => '',
-				'classes_widget' => 'titulo-pequeno tamanho-50',
-				'readmore' => '',
-				'posicao' => '',
-				'classes_posts' => 'thumb-quadrada exibicao-titulo  tamanho-50 foto-fundo',
-				'number' => 0
+				'tags_selecionadas' => '',
 			)
 		);
 		?>
 		<div class="form-container">
 			<?php
 			// campo de titulo "global"
-			$widget_title = sanitize_text_field( $instance['title'] );
+			$tags_selecionadas = sanitize_text_field( $instance['tags_selecionadas'] );
 			?>
 			<p>
-				<label>Titulo do widget</label>
-				<input class="widefat post-title" type="text" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr($widget_title); ?>">
+				<label>Tags por nome separadas por vírugula</label>
+				<input class="widefat post-title" type="text" name="<?php echo $this->get_field_name( 'tags_selecionadas' ); ?>" value="<?php echo esc_attr($tags_selecionadas); ?>">
 				<input type="text" class="force-change" name="<?php echo $this->get_field_name( 'force_change' );?>" style="display:none;"/>
 			</p>
-			<?php
-			// campo de classes "global"
-			$classes = sanitize_text_field( $instance['classes_widget'] );
-			?>
-			<p>
-				<label>
-					Classes CSS do widget<br>
-					<small>Coloque cada classe separado por uma barra de espaço</small>
-				</label>
-				<input class="widefat classes-css" type="text" name="<?php echo $this->get_field_name( 'classes_widget' ); ?>" value="<?php echo esc_attr($classes); ?>">
-			</p>
-			<?php
-			// seletor de termo da taxonomia "_eo_featured"
-			$posicao = absint( $instance['posicao'] );
-			$terms = get_terms( array( 'taxonomy' => '_featured_eo', 'hide_empty' => false ) );
-			?>
-			<p>
-				<label>
-					Posição (Precisa descrever melhor)<br>
-				</label>
-				<?php if( ! is_wp_error( $terms ) && ! empty( $terms ) ) : ?>
-					<select name="<?php echo $this->get_field_name( 'posicao' ); ?>" required>
-						<?php if ( 0 === $posicao ) {
-							echo '<option value="" selected>' . __( 'Selecione a taxonomia de posição' ) . '</option>';
-						} else {
-							echo '<option value="">' . __( 'Selecione a taxonomia de posição' ) . '</option>';
-						} ?>
-						<?php foreach ( $terms as $term ) : ?>
-							<?php $selected = '';?>
-							<?php if ( $posicao === $term->term_id ) {
-								$selected = 'selected';
-							} ?>
-							<option value="<?php echo $term->term_id;?>" <?php echo $selected;?>>
-								<?php echo $term->name;?>
-							</option>
-						<?php endforeach; ?>
-					</select>
-				<?php endif; ?>
-			</p>
-
-			<?php
-			// campo de classes "pra cada post"
-			$classes_posts = sanitize_text_field( $instance['classes_posts'] );
-			?>
-			<p>
-				<label>
-					Classes CSS do post<br>
-					<small>Coloque cada classe separado por uma barra de espaço</small>
-				</label>
-				<input class="widefat classes-css" type="text" name="<?php echo $this->get_field_name( 'classes_posts' ); ?>" value="<?php echo esc_attr($classes_posts); ?>">
-			</p>
-			<?php
-			// campo de classes "global"
-			$readmore = sanitize_text_field( $instance['readmore'] );
-			?>
-			<p>
-				<label>
-					Link Leia Mais
-				</label>
-				<input class="widefat" type="text" name="<?php echo $this->get_field_name( 'readmore' ); ?>" value="<?php echo esc_attr($readmore); ?>">
-			</p>
-
-			<?php
-			// numero de posts a ser exibido
-			$number = sanitize_text_field( $instance['number'] );
-			?>
-			<p>
-				<label>Numero de posts</label>
-				<input class="widefat" type="number" name="<?php echo $this->get_field_name( 'number' ); ?>" value="<?php echo esc_attr($number); ?>">
-			</p>
-
-		</div><!-- .form-container -->
 		<?php
 	}
 }
