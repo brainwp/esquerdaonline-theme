a67f3d024deb037450d19eedacca3120894eb5db
matheusgimenez <contato@matheusgimenez.com.br>
2018-06-28 11:09:24 -0300

    esconde as telas de remoção/edição da taxonomia 'colunistas'

--- inc/class-colunistas.php
+++ inc/class-colunistas.php
@@ -23,6 +23,9 @@
 		public function __construct() {
 			// Cria o termo relacionado ao colunista
 			add_action( 'save_post_colunistas', array( $this, 'create_term' ), 10, 3 );
+
+			// Esconde as telas de edição da taxonomia
+			add_action( 'admin_footer', array( $this, 'hide_taxonomy') );
 		}
 		/**
 		* Adiciona um termo com o nome do colunista em questão, quando o post do tipo colunista é salvo
@@ -36,6 +39,15 @@
 				return;
 			}
 			wp_insert_term( $post->post_title, 'colunistas', array( 'slug' => $post->post_name ) );
+		}
+		/**
+		 * Esconde as telas de edição/removação da taxonomia
+		 */
+		public function hide_taxonomy() {
+			$style = '';
+			$style .= 'a[href="edit-tags.php?taxonomy=colunistas"],div#colunistas-adder
+			{ display:none !important;}';
+			echo '<style type="text/css">' . $style . '</style>';
 		}
 		/**
 		 * Return an instance of this class.
