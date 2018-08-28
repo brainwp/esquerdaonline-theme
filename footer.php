1e385503c71e101789ad22c7051489e935736709
matheusgimenez <contato@matheusgimenez.com.br>
2018-08-28 14:46:23 -0300

    correções de css

--- footer.php
+++ footer.php
@@ -28,13 +28,20 @@
 
 					<?php if ( $links ) : ?>
 						<?php foreach( $links as $link ) : ?>
-							<?php $class = sprintf( 'fa-%s-%s', $link[ 'link_icon' ], $link[ 'link_icon' ][0] );?>
+							<?php $class = sprintf( 'fa-%s-f', $link[ 'link_icon' ] );?>
 							<?php if ( 'twitter' === $link[ 'link_icon'] ) {
 								$class = 'fa-twitter';
 							}
 							if ( 'instagram' === $link[ 'link_icon'] ) {
 								$class = 'fa-instagram';
 							}
+							if ( 'whatsapp' === $link[ 'link_icon'] ) {
+								$class = 'fa-whatsapp';
+							}
+							if ( 'telegram' === $link[ 'link_icon'] ) {
+								$class = 'fa-telegram-plane';
+							}
+
 							?>
 							<a href="<?php echo esc_url( $link[ 'link_url'] );?>">
 								<i class="fab <?php echo $class;?>"></i>
