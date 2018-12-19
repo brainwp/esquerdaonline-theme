9bf30ef07d0928df1de6dc5ed128da482191b307
matheusgimenez <contato@matheusgimenez.com.br>
2018-12-19 18:55:50 -0200

    remove acf do tema e passa a utilizar como plugin

--- functions.php
+++ functions.php
@@ -405,7 +405,6 @@ require_once get_template_directory() . '/inc/customizer.php';
 /**
  * Load Advanced Custom Fields
  */
-require_once get_template_directory() . '/inc/acf/acf.php';
 require_once get_template_directory() . '/inc/fields.php';
 
 /**
