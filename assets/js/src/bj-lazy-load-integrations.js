ca6f603370c2c6bb43fe8798964cd3b4766478d0
matheusgimenez <contato@matheusgimenez.com.br>
2019-01-02 14:49:33 -0200

    muda para 200px o 'range' da classe .foto-pequena

--- assets/js/src/bj-lazy-load-integrations.js
+++ assets/js/src/bj-lazy-load-integrations.js
@@ -3,10 +3,8 @@ jQuery(document).ready(function($) {
 		$( '.each-post-widget img' ).each(function() {
 			var $el = $( this );
 			//console.log( $( this ).attr( 'src' ) + ' : ' + $( this ).width() );
-			if ( ! ( parseInt( $el.width() ) > 150 ) ) {
+			if ( ! ( parseInt( $el.width() ) > 200 ) ) {
 				$el.parent().parent().parent().parent().parent().addClass( 'foto-pequena' );
-				console.log( parseInt( $el.width() ) );
-				console.log( 'qqw22ww');
 			}
 		});
 	});
