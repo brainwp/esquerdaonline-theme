0bdd9a7e6344eaffcbd46e9ceba870584d7f1eb4
matheusgimenez <contato@matheusgimenez.com.br>
2019-02-19 00:03:50 -0300

    issue #88

--- page-home.php
+++ page-home.php
@@ -8,7 +8,18 @@
  *
  * @package Odin
  * @since 2.2.0
- */
+ *
+$widgets_bk = get_option( 'sidebars_widgets_load' );
+$widgets = get_option( 'sidebars_widgets' );
+echo '<br>widgets bk:<br>';
+var_dump( $widgets_bk );
+echo '<br>widgets:<br>';
+$widgets[ 'widget__home-manchete_' ] = $widgets_bk[ 'widget__home-manchete_' ];
+$widgets[ 'widget__catastrofe_' ] = $widgets_bk[ 'widget__catastrofe_' ];
+$widgets[ 'widget__home-editorial_' ] = $widgets_bk[ 'widget__homes-editorial_' ];
+$widgets[ 'widget__home-video-na-manchete_' ] = $widgets_bk[ 'widget__home-video-na-manchete_'];
+update_option( 'sidebars_widgets', $widgets );
+*/
 get_header( 'large' ); ?>
 	<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
 		<div class="col-md-12 no-padding">
