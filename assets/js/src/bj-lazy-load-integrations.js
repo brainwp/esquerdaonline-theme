c5c707de5f4593e3e61aa4ba52f692a3c27ae707
matheusgimenez <contato@matheusgimenez.com.br>
2019-03-02 17:34:54 -0300

    corrige redes sociais em imagens pequenas (.foto-pequena)

--- assets/js/src/bj-lazy-load-integrations.js
+++ assets/js/src/bj-lazy-load-integrations.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
 			var $el = $( this );
 			//console.log( $( this ).attr( 'src' ) + ' : ' + $( this ).width() );
 			if ( ! ( parseInt( $el.width() ) > 200 ) ) {
-				$el.parent().parent().parent().parent().parent().addClass( 'foto-pequena' );
+				$el.parent().parent().parent().parent().addClass( 'foto-pequena' );
 			}
 		});
 	});
